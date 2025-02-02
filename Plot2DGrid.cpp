370a5fc95258838b468ac7b601eb8710fc1a12e8
techadvision <157640325+techadvision@users.noreply.github.com>
2025-02-02 14:21:16 +0100

    Ability to modify units, and some size adjustments, for grid

--- Plot2DGrid.cpp
+++ Plot2DGrid.cpp
@@ -2,7 +2,7 @@
 #include <QObject>
 
 
-Plot2DGrid::Plot2DGrid() : angleVisibility_(false)
+Plot2DGrid::Plot2DGrid() : angleVisibility_(false), isMetric_(true)
 {}
 
 
@@ -13,16 +13,25 @@ bool Plot2DGrid::draw(Canvas& canvas, Dataset* dataset, DatasetCursor cursor)
 
     QPen pen(_lineColor);
     pen.setWidth(_lineWidth);
+    //Changed, set the color
+    //pen.setColor(QColor(255, 255, 255));
 
     QPainter* p = canvas.painter();
     p->setPen(pen);
-    p->setFont(QFont("Asap", 14, QFont::Normal));
+    //Changed increased the font size 14 to 26
+    p->setFont(QFont("Asap", 20, QFont::Bold));
     QFontMetrics fm(p->font());
 
     const int imageHeight{ canvas.height() }, imageWidth{ canvas.width() },
         linesCount{ _lines }, textXOffset{ 30 }, textYOffset{ 10 };
 
+    float conversionFactor = 1.0; // Default to metric (meters)
+    if (!isMetric_) {
+        conversionFactor = 3.28084; // Convert to feet if not metric
+    }
+
     for (int i = 1; i < linesCount; ++i) {
+
         const int posY = i * imageHeight / linesCount;
 
         QString lineText;
@@ -41,7 +50,17 @@ bool Plot2DGrid::draw(Canvas& canvas, Dataset* dataset, DatasetCursor cursor)
         if (cursor.distance.isValid()) { // depth
             const float distFrom{ cursor.distance.from }, distTo{ cursor.distance.to },
                 distRange{ distTo - distFrom }, rangeVal{ distRange * i / linesCount + distFrom };
-            lineText.append( { QString::number(rangeVal, 'f', 2) + QObject::tr(" m") } );
+            //Support for imperial and metric
+            // Convert to imperial if needed (multiply by conversion factor)
+            float finalRangeVal = rangeVal * conversionFactor;
+            if (isMetric_) {
+                //Changed to 1 decimal
+                lineText.append( { QString::number(finalRangeVal, 'f', 1) + QObject::tr(" m") } );
+            } else {
+                //Changed to 1 decimal
+                lineText.append( { QString::number(finalRangeVal, 'f', 1) + QObject::tr(" ft") } );
+            }
+
         }
 
         if (isFillWidth())
@@ -54,9 +73,16 @@ bool Plot2DGrid::draw(Canvas& canvas, Dataset* dataset, DatasetCursor cursor)
     }
 
     if (cursor.distance.isValid()) {
-        p->setFont(QFont("Asap", 26, QFont::Normal));
-        float val{ cursor.distance.to };
-        QString range_text = QString::number(val, 'f', (val == static_cast<int>(val)) ? 0 : 2) + QObject::tr(" m");
+        p->setFont(QFont("Asap", 26, QFont::Bold));
+        //Support for metric and imperial
+        float val{ cursor.distance.to * conversionFactor };
+        //Changed to 1 decimal
+        QString range_text = "";
+        if (isMetric_) {
+            range_text = QString::number(val, 'f', (val == static_cast<int>(val)) ? 0 : 1) + QObject::tr(" m");
+        } else {
+            range_text = QString::number(val, 'f', (val == static_cast<int>(val)) ? 0 : 1) + QObject::tr(" ft");
+        }
         p->drawText(imageWidth - textXOffset / 2 - range_text.count() * 25, imageHeight - 10, range_text);
     }
 
@@ -73,9 +99,11 @@ bool Plot2DGrid::draw(Canvas& canvas, Dataset* dataset, DatasetCursor cursor)
         if (isfinite(distance)) {
             pen.setColor(QColor(250, 100, 0));
             p->setPen(pen);
-            p->setFont(QFont("Asap", 40, QFont::Normal));
+            //Changed to bold and font 40 to 46
+            p->setFont(QFont("Asap", 46, QFont::Bold));
             float val{ round(distance * 100.f) / 100.f };
-            QString rangeText = QString::number(val, 'f', (val == static_cast<int>(val)) ? 0 : 2) + QObject::tr(" m");
+            //Changed to 1 decimal
+            QString rangeText = QString::number(val, 'f', (val == static_cast<int>(val)) ? 0 : 1) + QObject::tr(" m");
             p->drawText(imageWidth / 2 - rangeText.count() * 32, imageHeight - 15, rangeText);
         }
     }
@@ -87,3 +115,11 @@ void Plot2DGrid::setAngleVisibility(bool state)
 {
     angleVisibility_ = state;
 }
+
+// Pulse
+void Plot2DGrid::setMeasuresMetric(bool metric)
+{
+    isMetric_ = metric;
+}
+
+
